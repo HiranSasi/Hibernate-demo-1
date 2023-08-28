3616e78b13a7c9860ecdb478e94e8a9f79d93c0f
HiranSasi <hiransasitha123@gmail.com>
2023-08-28 20:34:34 +1200

    session configuratin java

---
+++ src/utill/SessionFactoryConfiguration.java
@@ -0,0 +1,15 @@
+package utill;
+
+public class SessionFactoryConfiguration {
+    private SessionFactoryConfiguration serviceFactoryConfiguration;
+    
+    private SessionFactoryConfiguration(){}
+
+    public static SessionFactoryConfiguration getInstance(){
+
+        
+
+
+
+    }
+}
